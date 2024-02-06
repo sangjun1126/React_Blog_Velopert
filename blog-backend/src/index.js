f57a9ffc3dfc30775a9bc0a10b65af3a2100ebf3
sangjun1126 <jeongsangjun1234@gmail.com>
2024-02-06 12:14:07 +0900

    koa finish

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,11 +1,14 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 const api = require('./api');
 const app = new Koa();
 const router = new Router();
 
 // 라우터 설정하기
 router.use('/api', api.routes()); // api 라우트 적용
+
+app.use(bodyParser());
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
