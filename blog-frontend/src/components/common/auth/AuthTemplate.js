4bfcd47314fc99e4665fe336aacbe0436cb640af
sangjun1126 <jeongsangjun1234@gmail.com>
2024-02-07 16:12:54 +0900

    fe first setting

---
+++ blog-frontend/src/components/common/auth/AuthTemplate.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+// 회원가입 / 로그인 페이지의 레이아웃을 담당하는 컴포넌트 입니다.
+
+const AuthTemplateBloack = styled.div``;
+
+const AuthTemplate = () => {
+    return (
+        <AuthTemplateBloack>
+
+        </AuthTemplateBloack>
+    )
+}
+
+export default AuthTemplate;
--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from '../node_modules/react-router-dom/dist/index';
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules/index';
 
+const store = createStore(rootReducer, composeWithDevTools());
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store = {store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
